1350824aa56aa0d4a45be69a7bdb4a486f567c5c
jmyooelice <131931853+jmyooelice@users.noreply.github.com>
2023-05-03 16:07:23 +0900

    Create result.js

---
+++ result.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const resultElement = document.getElementById('result');
+  const restartButton = document.getElementById('restart-button');
+
+  // Check if the mbti_result key exists in local storage
+  if (localStorage.getItem('mbti_result')) {
+    const mbtiResult = localStorage.getItem('mbti_result');
+    resultElement.textContent = `${mbtiResult}입니다!`;
+  } else {
+    resultElement.textContent = '아직 테스트를 진행하지 않았습니다!';
+    restartButton.textContent = '테스트 하러 가기';
+    restartButton.addEventListener('click', function () {
+      window.location.href = './index.html';
+    });
+  }
+});
